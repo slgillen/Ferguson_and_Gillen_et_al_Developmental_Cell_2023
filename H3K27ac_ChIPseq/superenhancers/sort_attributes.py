eeb72825d7344fd0c3865a253fedb50c1a8e876a
slgillen <62572282+slgillen@users.noreply.github.com>
2022-11-28 15:17:39 +0000

    Update sort_attributes.py

--- H3K27ac_ChIPseq/superenhancers/sort_attributes.py
+++ H3K27ac_ChIPseq/superenhancers/sort_attributes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python 
 
 import sys
-currentGFF = sys.argv[1]
+currentBED = sys.argv[1]
 newGFF = sys.argv[2]
 
 
@@ -9,15 +9,17 @@ import os
 import subprocess
 
 
-with open(currentGFF) as infile:
+with open(currentBED) as infile:
     with open(newGFF, 'w') as outfile:
         for line in infile:
             cols = line.split("\t")
-            #NOTE: there are empty columns
-            indices=[3,4]
+            indices=[0,1,2]
             newcols=[cols[index] for index in indices]
             newline=cols[0]+'\t'+'ROSE_STITCHED_LOCI'+'\t'+'region'+'\t'
-            newline=newline+'\t'.join(newcols)+'\t'+'.'+'\t'+'.'+'\t'+'.'+'\t'+'region_id '+'\"'+str(cols[8].strip('\n'))+'\"'+'\n'
+            newline=newline+cols[1]+'\t'+str(cols[2].strip('\n'))+'\t'+'.'+'\t'+'.'+'\t'+'.'+'\t'
+            newcols[2]=str(newcols[2].strip('\n'))
+            attr='_'.join(newcols)
+            newline=newline+'region_id '+'\"'+attr+'\"'+'\n'
             outfile.write(newline)
 
 infile.close()

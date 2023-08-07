af4b81cd18385b80d4053849c22ab3b5b9017687
slgillen <62572282+slgillen@users.noreply.github.com>
2023-08-07 14:41:20 +0100

    Update peak_annotation.R

--- H3K27ac_ChIPseq_PBandRA/peak_annotation.R
+++ H3K27ac_ChIPseq_PBandRA/peak_annotation.R
@@ -1,66 +1,31 @@
 #!/usr/bin/env Rscript
 
 
-################ load libraries ###################
+# load libraries ------------------------------------------
 
 library(ChIPseeker)
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 library(clusterProfiler)
-library(EnsDb.Hsapiens.v75)
 
 
-
-
-txdb1 <- TxDb.Hsapiens.UCSC.hg19.knownGene
-
-indir<-'' #directory with input files
-outdir<-'' #directory for outputs
-
-conditions<-c('RAvDMSO','PBvDMSO','PBRAvDMSO','PBvRA','PBRAvPB','PBRAvRA')
-grouptypes<-c('_up05','_ns05','_down05')
-
-########### multi plots ############
+# plot peak annotation information for H3K27ac response groups ------------------------------------------
+indir<-'' #input directory
+outdir<-'' #output directory
 
 plot_ChIPseeker_multiple<-function(txdbX,filenamesX,plotnameX,datadir1,datadir2){
   peakFiles<-lapply(filenamesX, function(filename) readPeakFile(paste0(datadir1,filename,'.bed')))
   names(peakFiles)<-filenamesX
   peakAnnoList <- lapply(peakFiles, annotatePeak, TxDb=txdbX,tssRegion=c(-3000, 3000), verbose=FALSE)
-  tiff(paste0(datadir2,'peakAnno_',plotnameX,'.tiff'),height = 10, width = 20, units='cm', compression = "lzw", res = 300)
+  tiff(paste0(datadir2,'peakAnno_',plotnameX,'.tiff'),height = 18, width = 15, units='cm', compression = "lzw", res = 300)
   print(plotAnnoBar(peakAnnoList))
   dev.off()
 
-  tiff(paste0(datadir2,'distToTSS_',plotnameX,'.tiff'),height = 10, width = 20, units='cm', compression = "lzw", res = 300)
+  tiff(paste0(datadir2,'distToTSS_',plotnameX,'.tiff'),height = 18, width = 15, units='cm', compression = "lzw", res = 300)
   print(plotDistToTSS(peakAnnoList))
   dev.off()
 }
 
-for(cond in conditions){
-    filenames<-paste0(cond,grouptypes)
-    plot_ChIPseeker_multiple(txdb1,filenames,cond,indir,outdir)
-}
+txdb1 <- TxDb.Hsapiens.UCSC.hg19.knownGene
+filenames<-c("down_all3","down_RA_PBRA","down_PB_PBRA","down_PBRA","down_RA","down_PB","down_PB_RA","ns_all3","PBdown_RAup_PBRAdown","PBdown_RAup_PBRAns","PBdown_RAup_PBRAup","PBup_RAdown_PBRAdown","PBup_RAdown_PBRAns","PBup_RAdown_PBRAup","up_PB_RA","up_PB","up_RA","up_PBRA","up_PB_PBRA","up_RA_PBRA","up_all3")
 
-
-########### peak annotations per condition set ############
-
-for(cond in conditions){
-    filenames<-paste0(cond,grouptypes)
-    for(f in filenames){
-        peak <- readPeakFile(paste0(indir,f,'.bed'))
-        print(peak)
-
-        txdb1 <- TxDb.Hsapiens.UCSC.hg19.knownGene
-        seqlevelsStyle(txdb1) <- "UCSC"
-        peakAnno.txdb <- annotatePeak(peak, tssRegion=c(-3000, 3000),TxDb=txdb1, annoDb="org.Hs.eg.db")
-        peakAnno.txdb2<-data.frame(peakAnno.txdb,stringsAsFactors=FALSE)
-        #peakAnno.txdb2<-apply(peakAnno.txdb2,2,function(x) gsub('\t','',x))
-        peakAnno.txdb2$annotation<-gsub('\t','',peakAnno.txdb2$annotation)
-        write.table(peakAnno.txdb2,paste0(outdir,'peakAnno_',f,'.txt'),quote=FALSE,col.names=TRUE,row.names=FALSE,sep='\t')
-        rm(peak,txdb1,peakAnno.txdb,peakAnno.txdb2)
-    }
-}
-
-
-
-
-
-
+plot_ChIPseeker_multiple(txdb1,filenames,'ChIPseeker',indir,outdir)
